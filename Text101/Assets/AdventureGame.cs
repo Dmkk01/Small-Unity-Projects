98666f642c075b864bb8a07111e19d1afbf209c9
Dominik <dmkk1@outlook.com>
2021-06-02 00:19:26 +0300

    Upload All States

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -16,6 +16,21 @@ public class AdventureGame : MonoBehaviour
 
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState() {
+        var nextStates = state.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
     }
 }
