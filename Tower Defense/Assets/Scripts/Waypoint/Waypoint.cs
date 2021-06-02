98baaef231e49253f87d615e44bb38eb51c36ca6
Dominik <dmkk1@outlook.com>
2021-06-02 21:07:40 +0300

    Add Spawner

---
+++ Tower Defense/Assets/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    [SerializeField] private Vector3[] points;
+    private Vector3 _currentPosition;
+    private bool _gameStart;
+
+
+
+    public Vector3[] Points => points;
+    public Vector3 CurrentPosition => _currentPosition;
+    
+    void Start()
+    {
+        _gameStart = true;
+        _currentPosition = transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos() {
+
+        if (!_gameStart && transform.hasChanged) {
+            _currentPosition = transform.position;
+
+        }
+
+        for (int i =0; i < points.Length; i++ ) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(points[i] + _currentPosition, 0.5f);
+
+            if(i < points.Length - 1) {
+                Gizmos.color = Color.grey;
+                Gizmos.DrawLine(points[i] + _currentPosition, points[i+1] + _currentPosition);
+            }
+        }
+    }
+
+}
