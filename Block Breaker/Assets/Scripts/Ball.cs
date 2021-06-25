3ce82c8f1c2966a27a8782497c9c4b45bf581cca
Dominik <dmkk1@outlook.com>
2021-06-25 11:43:50 +0300

    Add Ball Interaction

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float velocityX;
+    [SerializeField] float velocityY;
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LaunchOnClick();
+        }
+    }
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnClick() {
+        if(Input.GetMouseButtonDown(0)) {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+        }
+    }
+}
